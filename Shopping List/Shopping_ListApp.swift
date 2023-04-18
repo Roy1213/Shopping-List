3e64d6a7841351beb51c72adcd0b4a1c62391ec5
Roy Alameh <roy.alameh24@d155.org>
2023-04-18 07:49:55 -0500

    Initial Commit

---
+++ Shopping List/Shopping_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_ListApp.swift
+//  Shopping List
+//
+//  Created by ROY ALAMEH on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
